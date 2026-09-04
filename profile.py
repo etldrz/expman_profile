0f698a78a0a7a57507d8c8eda18745ee33d54ef0
Evan Zook <evan.zook@utah.edu>
2026-09-04 12:54:10 -0600

    typo

--- profile.py
+++ profile.py
@@ -7,7 +7,7 @@ import geni.portal as portal
 import geni.rspec.pg as rspec
 
 portal.context.defineParameter("n", "Number of VMs", 
-                               portal.ParameterType.Interger, 1)
+                               portal.ParameterType.Integer, 1)
 
 params = portal.context.bindParameters()
 
