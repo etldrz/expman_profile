0e2931c4679e771da8ab188f13d187ff74ddad4a
Evan Zook <evan.zook@utah.edu>
2026-09-18 13:47:48 -0600

    mise install, then iex w custom hostname

--- setup.sh
+++ setup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-cd /local
-chown +x "${USER}:${GROUP}"
-echo "${USER}:${GROUP}" >> /local/here.txt
+#echo "${USER}:${GROUP}" >> /local/here.txt
+ls /users > /local/here.txt
 
 curl https://mise.run | sh
-echo "eval \"\$($HOME/.local/bin/mise activate bash)\"" >> "$HOME/.bashrc"
-eval "$(HOME/.local/bin/mise activate bash)"
+#echo "eval \"\$($HOME/.local/bin/mise activate bash)\"" >> "$HOME/.bashrc"
+eval "$($HOME/.local/bin/mise activate bash)"
 git clone https://github.com/etldrz/caring.git /local/expman
 cd /local/expman
-sudo mise install
+mise install
+mix local.hex --if-missing --force
+mix deps.get
+node_name=$(hostname | sed 's/\..*//')
+iex -S mix --sname "$node_name" --cookie salsa
