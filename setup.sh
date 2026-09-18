a5a08f7a4b58de885dc084eaf8c3fb7258b8883a
Evan Zook <evan.zook@utah.edu>
2026-09-18 11:30:49 -0600

    testing mise install via geni

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "This is going to make a file" >> "$HOME/test.tx"
+#echo "This is going to make a file" >> "$HOME/test.tx"
 
-sudo curl https://mise.run | sh
-sudo echo "eval \"\$($HOME/.local/bin/mise activate bash)\"" >> "$HOME/.bashrc"
-sudo git clone https://github.com/etldrz/caring.git /local/expman
-cd /local/expman
-sudo mise install
+#sudo curl https://mise.run | sh
+#sudo echo "eval \"\$($HOME/.local/bin/mise activate bash)\"" >> "$HOME/.bashrc"
+git clone https://github.com/etldrz/caring.git /local/expman
+#cd /local/expman
+#sudo mise install
