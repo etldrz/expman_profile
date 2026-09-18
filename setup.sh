197a2a596bb0112b232e79bf716671d9a8f07d33
Evan Zook <evan.zook@utah.edu>
2026-09-18 15:12:57 -0600

    fixed issue with mix

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 #echo "${USER}:${GROUP}" >> /local/here.txt
-ls /users > /local/here.txt
+#ls /users > /local/here.txt
 
 curl https://mise.run | sh
 #echo "eval \"\$($HOME/.local/bin/mise activate bash)\"" >> "$HOME/.bashrc"
 eval "$($HOME/.local/bin/mise activate bash)"
 git clone https://github.com/etldrz/caring.git /local/expman
 cd /local/expman
+sudo chown "$USER" .
 mise install
 mix local.hex --if-missing --force
 mix deps.get
 node_name=$(hostname | sed 's/\..*//')
-iex -S mix --sname "$node_name" --cookie salsa
+iex --sname "$node_name" --cookie salsa -S mix 
